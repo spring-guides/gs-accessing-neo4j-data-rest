104d2a4327f2908581cd2dfc157d70f8c0f48b9c
Greg Turnquist <gturnquist@gopivotal.com>
2014-03-03 12:40:57 -0500

    Add custom finder and revise text

--- complete/src/main/java/hello/PersonRepository.java
+++ complete/src/main/java/hello/PersonRepository.java
@@ -1,9 +1,14 @@
 package hello;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import java.util.List;
+
+import org.springframework.data.neo4j.repository.GraphRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
-public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+public interface PersonRepository extends GraphRepository<Person> {
+
+	List<Person> findByLastName(@Param("name") String name);
 
 }
