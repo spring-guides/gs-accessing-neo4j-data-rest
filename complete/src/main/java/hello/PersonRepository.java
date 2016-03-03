8971d3135c506dc7258fa12ba6cb0afced33e279
Matthias Sieber <matthiasksieber@gmail.com>
2016-03-03 12:51:24 -0800

    find by last name

--- complete/src/main/java/hello/PersonRepository.java
+++ complete/src/main/java/hello/PersonRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
 
-    // TODO: Replace with @Param("name") when Spring Data Neo4j supports names vs. positional arguments
-    List<Person> findByLastName(@Param("0") String name);
+    List<Person> findByLastName(@Param("name") String name);
 
 }
