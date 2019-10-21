48c214db205ae731a44d934fd01044c3493073bf
Jay Bryant <jbryant@pivotal.io>
2019-10-21 13:39:34 -0500

    Upgrade to Spring Boot 2.2
    I upgraded to Spring Boot 2.2. That required upgrading JUnit to version 5 and upgrading the Gradle wrappers.

---
+++ initial/src/test/java/com/example/accessingneo4jdatarest/AccessingNeo4jDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accessingneo4jdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingNeo4jDataRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
