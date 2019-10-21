8d9d18c9def5fdcfe61c8cad9a1bfcb68c60e5fe
Jay Bryant <jbryant@pivotal.io>
2019-10-21 13:39:34 -0500

    Upgrade to Spring Boot 2.2
    I upgraded to Spring Boot 2.2. That required upgrading JUnit to version 5 and upgrading the Gradle wrappers.

--- complete/src/test/java/com/example/accessingneo4jdatarest/AccessingNeo4jDataRestApplicationTests.java
+++ complete/src/test/java/com/example/accessingneo4jdatarest/AccessingNeo4jDataRestApplicationTests.java
@@ -16,16 +16,14 @@
 
 package com.example.accessingneo4jdatarest;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.data.neo4j.Neo4jDataAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -34,7 +32,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @Import(Neo4jDataAutoConfiguration.class)
@@ -46,7 +43,7 @@ public class AccessingNeo4jDataRestApplicationTests {
 	@Autowired
 	private PersonRepository personRepository;
 
-	@Before
+	@BeforeEach
 	public void deleteAllBeforeTests() throws Exception {
 		personRepository.deleteAll();
 	}
