22031c60080bf6f6b0d09bb06679afac19123ece
Greg Turnquist <gturnquist@pivotal.io>
2019-11-26 13:07:26 -0600

    Switch to tabs, and adjust tabsizing in asciidoctor.

--- complete/src/main/java/com/example/accessingneo4jdatarest/PersonRepository.java
+++ complete/src/main/java/com/example/accessingneo4jdatarest/PersonRepository.java
@@ -9,6 +9,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
 
-    List<Person> findByLastName(@Param("name") String name);
+	List<Person> findByLastName(@Param("name") String name);
 
 }
